c13a14322e290022ee509a6fc4f0a90e73b7aad7
vagif4799 <quliyev_vaqif@icloud.com>
2020-01-18 10:01:41 +0400

    -- Created JFrame and added JLabel

---
+++ src/main/java/step0/Application.java
@@ -0,0 +1,28 @@
+package step0;
+import javax.swing.*;
+
+public class Application {
+
+    public Application() {
+
+        JFrame jFrame = new JFrame("Vagif's First Swing App");
+        jFrame.setSize(275, 100);
+        jFrame.setDefaultCloseOperation(jFrame.EXIT_ON_CLOSE);
+        JLabel jLabel = new JLabel("Vagif Says Hello... :)");
+        jFrame.add(jLabel);
+        jFrame.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Application();
+            }
+        });
+
+    }
+
+}
